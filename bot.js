9f1cdcd89cb0292d63e0df290b158a2a58b34db5
a7ssan <44026213+a7ssan@users.noreply.github.com>
2019-01-26 08:57:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,49 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
+const discord = require("discord.js");
+const randomWord = require("random-words")
+const client = new discord.Client();
 
+client.login(process.env.TOKEN);
+  
 
-client.on("ready", () => {
-let channel =     client.channels.get("526521844040335362")
-setInterval(function() {
-channel.send(راح أجيب نيترو);
-}, 30)
+ 
+var spam;
+ 
+var phrases = ["thing1","another one","keep adding more","there's no limit"];
+ 
+function startspam()
+{
+    console.log("Spam starting!")
+    var server = client.guilds.get("538592060580691968");
+    var chan = new discord.TextChannel(server,{"id":"538592060580691970"});
+    spam = client.setInterval(()=>
+    {
+        chan.send(randomWord()).then(msg=>{ // Sticking with randomwords.
+            console.log(msg.content);
+        });
+  
+    },5000);
+}
+ 
+function stopspam()
+{
+    client.clearTimeout(spam);
+    console.log("Spam Stopped.")
+}
+ 
+client.on("ready",()=>{
+    console.log("Ready!");
 })
-
-client.login(process.env.BOT_TOKEN);
+ 
+client.on("message",msg=>{
+    if(msg.author.id == "531150470681460746")
+    {
+        if(msg.content.toLowerCase() == "=start")
+        {
+            startspam()
+        }
+        else if(msg.content.toLowerCase() == "=stop")
+        {
+            stopspam()
+        }
+    }
+})
